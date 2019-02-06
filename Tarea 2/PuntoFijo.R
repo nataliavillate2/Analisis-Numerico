3789f3487d4faa08e4d523611d04efed4829b679
nataliavillate2 <46963965+nataliavillate2@users.noreply.github.com>
2019-02-05 21:46:11 -0500

    Add files via upload

--- Tarea 2/PuntoFijo.R
+++ Tarea 2/PuntoFijo.R
@@ -1,43 +1,21 @@
-rm(list=ls())
-gx = function(x) ((exp(x))/(pi))
-fx = function(x) (exp(x)-pi*x)
-y = function(x) x
-x = seq(0,1,0.1);
-
-puntoFijo = function(a,b,error) {
-  
-  
-  if((gx(a)<a) || (gx(b)>b)){ 
-    cat("El Intervalo: [ ", a,",", b , "] no es valido\n")
-  }else{
-    
-    puntoMedio = (a+b)/2;
-    iteraciones = 0;
-    xInicial = puntoMedio
-    funcionXInicial = gx(xInicial);
-    check = 0;
-    
-    while(abs(xInicial-funcionXInicial)>error){
-      iteraciones = iteraciones + 1
-      
-      if(xInicial < a){
-        check = 1;
-      }
-      if(check == 0){
-        xInicial = funcionXInicial;
-        funcionXInicial = gx(xInicial);
-      }else{
-        funcionXInicial = xInicial;
-        xInicial = y(funcionXInicial);
-      }
-      cat("x: ", xInicial, " con: ", iteraciones ," Iteraciones\n")
-    }
-    cat("Iteraciones =",iteraciones,"Resultado = ",xInicial,"\n")
-  }
-  
-}
-
-puntoFijo(0,1,10e-8)
-curve(gx,-2,2, col="red")
-curve(y,-2,2,col="blue", add = "TRUE")
-matplot(x,cbind(gx(x),y(x)),type="l",col=c("blue","red"))
+fx <- function(x) ((exp(x)) - (pi*x))
+> gx <- function(x) ((exp(x))-pi)
+> 
+  > newton <- function(a,b,error) {
+    +     
+      +     puntoMedio = (a+b)/2;
+      +     iteraciones = 0
+      +     
+        +     repeat{
+          +         cc = fx(puntoMedio)/gx(puntoMedio)
+          +         x = puntoMedio - cc
+          +         dx = abs(cc)
+          +         puntoMedio = x
+          +         iteraciones = iteraciones+1
+          +         if(dx <= error ) break;
+          +         cat("x: ", x, " con: ", iteraciones ," Iteracion\n")
+          +     }
+      +     cat("Iteraciones = ", iteraciones, " Resultado = ", x, "\n")
+      + }
+  > 
+    > newton(0,1,10e-8)
